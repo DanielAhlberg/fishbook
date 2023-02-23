4dfac244409f9243e50a7b24c398137f443feb74
JoelBergstrand <joel.bergstrand@hotmail.com>
2023-02-23 14:16:40 +0100

    Added header and footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,10 @@
 <script>
-    import {
-        Footer,
-        FooterCopyright,
-        FooterLinkGroup,
-        FooterLink,
-        Alert,
-        Navbar,
-        NavBrand,
-        NavLi,
-        NavUl,
-        NavHamburger,
-    } from "flowbite-svelte";
+    import Header from "../lib/Header.svelte";
+    import Footer from "../lib/Footer.svelte";
+    import { Alert } from "flowbite-svelte";
 </script>
 
-<Navbar let:hidden let:toggle>
-    <NavBrand href="/">
-        <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-        />
-        <span
-            class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-        >
-            Fishbook
-        </span>
-    </NavBrand>
-    <NavHamburger on:click={toggle} />
-    <NavUl {hidden}>
-        <NavLi href="/" active={true}>Home</NavLi>
-        <NavLi href="/about">About</NavLi>
-        <NavLi href="/services">Services</NavLi>
-        <NavLi href="/pricing">Pricing</NavLi>
-        <NavLi href="/contact">Contact</NavLi>
-    </NavUl>
-</Navbar>
+<Header />
 <div class="p-8">
     <h1>Welcome to Fishbook</h1>
     <p>
@@ -45,17 +15,4 @@
         submitting again.
     </Alert>
 </div>
-<Footer>
-    <FooterCopyright href="/" by="Flowbite™" year={2022} />
-    <FooterLinkGroup
-        ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
-    >
-        <FooterLink href="/">About</FooterLink>
-        <FooterLink href="/">Privacy Policy</FooterLink>
-        <FooterLink href="/">Licensing</FooterLink>
-        <FooterLink href="/">Contact</FooterLink>
-    </FooterLinkGroup>
-</Footer>
-
-<style>
-</style>
+<Footer />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Header from "../../lib/Header.svelte";
+    import Footer from "../../lib/Footer.svelte";
+</script>
+
+<Header />
+<h1>Hello</h1>
+<Footer />
