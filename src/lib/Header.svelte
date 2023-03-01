a75150b27960914c747e544565bb06af29ec2dc5
biancawidstam <biancaizajosefin.widstam@student.kuleuven.be>
2023-03-01 13:42:29 +0100

    right links

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -24,9 +24,8 @@
     <NavHamburger on:click={toggle} />
     <NavUl {hidden}>
         <NavLi href="/" active={true}>Home</NavLi>
-        <NavLi href="/about">About</NavLi>
-        <NavLi href="/services">Services</NavLi>
-        <NavLi href="/pricing">Pricing</NavLi>
-        <NavLi href="/contact">Contact</NavLi>
+        <NavLi href="/add-catch">Add catch</NavLi>
+        <NavLi href="/catalogue">Fish catalogue</NavLi>
+        <NavLi href="/search">Fish search</NavLi>
     </NavUl>
 </Navbar>
