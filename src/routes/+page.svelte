4090f4f8677c7ce8698f89f1f0bcbb0a90f552da
Lisa Bybro <li1702by-s@student.lu.se>
2023-03-03 10:13:24 +0100

    text if the fish is boring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,24 +4,30 @@
     import { onMount } from "svelte";
     import { Alert, Label, Select } from "flowbite-svelte";
     import { Card, Button, Toggle } from "flowbite-svelte";
-    let hCard = false;
+    let vCard = false;
     let selected = "";
     let fishes = [{ value: "", name: "" }];
     let endpoint = "https://www.fishwatch.gov/api/species";
-    let fishOfTheDay = {value: "", name: ""};
+    let randomFish = {value: "", name: ""};
     let fishInfo = [{}];
-    let imgSrc = "";
+    let imgSrc = "./fish_logga.png";
 
     async function randomizeFish() {
-        fishOfTheDay = fishes[Math.floor(Math.random()*fishes.length)];
-        if (fishOfTheDay.value) {
-            const uri = encodeURI(endpoint + "/" + fishOfTheDay.value.toLowerCase());
+        randomFish = fishes[Math.floor(Math.random()*fishes.length-1)];
+        setImgSrc(randomFish.name);
+        console.log(randomFish["Species Name"]);
+    }
+
+    async function setImgSrc(fishName) {
+        if (fishName) {
+            const uri = encodeURI(endpoint + "/" + fishName.toLowerCase());
             const response = await fetch(uri);
             const data = await response.json();
             fishInfo = data[0];
-            imgSrc =    fishInfo["Image Gallery"]
-                        ? fishInfo["Image Gallery"][1]["src"]
-                        : "";
+            let gallery = fishInfo["Image Gallery"];
+            imgSrc =    gallery
+                        ? gallery[Math.floor(Math.random()*gallery.length-1)]["src"]
+                        : "./fish_logga.png";
         }
     }
 
@@ -39,12 +45,15 @@
     function formatInfo() {
         let scientificName =        fishInfo["Scientific Name"] || "";
         let location =              fishInfo["Location"] || "";
-        let physicalDescription =   fishInfo["Physical Description"] || "";
+        let taste =                 fishInfo["Taste"] || "";
+        let infoString = (scientificName + '\n' + location + '\n' + taste).replace(/<\/?[^>]+(>|$)/g, "").replace(/\&nbsp;/g, '');
+        return ((infoString.length > 4) ? infoString : "There are no fun facts about this fish :(");
+    }
 
-        let infoString = (scientificName + '\n' + location + '\n' + physicalDescription).replace(/<\/?[^>]+(>|$)/g, "").replace(/\&nbsp;/g, '');
+    function setText() {
+        let infoString = formatInfo();
+        (infoString.length < 1) ? infoString = "There is no fun fact about this fish" : "";
         return infoString;
-
-
     }
 
     onMount(async function () {
@@ -66,14 +75,19 @@
 
 <Header />
 <div class="px-20 py-8">
-    <h1>Welcome to Fishbook</h1>
-    <div>
+    <div class=" flex justify-center pb-2">
+        <Card href="/addcatch">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Welcome to FishBook</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+              This is your digital Fishing Club. Log your new catches and take a trip down memory lane and review your old ones.
+            </p>
+          </Card>
         {#if Object.keys(fishes).length > 0}
             {#await preload(imgSrc) then _}
-                <Card img={imgSrc} href="/" horizontal reverse={hCard}>
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fish of the day!</h5>
+                <Card img={imgSrc} href="/" reverse={vCard} >
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fun facts about {randomFish.name}</h5>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-                    The fish of the day {fishOfTheDay.name}. {formatInfo()}
+                        {formatInfo()}
                   </p>
                 </Card>
             {/await}
