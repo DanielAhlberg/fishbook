6b45f0c76a3603048f67b7d213276d423c2e06d0
Daniel Ahlberg <daniel.ahlberg.4255@student.lu.se>
2023-03-08 14:58:28 +0100

    Fixed catalogue

--- src/routes/catalogue/+page.svelte
+++ src/routes/catalogue/+page.svelte
@@ -1,45 +1,63 @@
 <script>
-    let images =[];
-    import { Button, Modal, List, Li } from "flowbite-svelte";
+    import { fishes } from "../../storable.js";
+    import { Button, Modal } from "flowbite-svelte";
     import Wrapper from "$lib/Wrapper.svelte";
-    var modals = new Array(images.length).fill(false);
+    var modals = new Array(Object.keys(fishes).length).fill(false);
+    let local = {};
+
+    if (typeof window !== "undefined") {
+        if (localStorage["fishes"]) {
+            local = JSON.parse(localStorage["fishes"]);
+        }
+    }
 </script>
 
 <Wrapper>
+    <p class="text-xl font-bold text-center text-gray-900 dark:text-white">
+        Du har fångat {Object.keys(local).length} fiskar
+    </p>
     <div class="grid grid-cols-3 gap-8 m-16">
-        {#each images as image, i}
+        {#each Object.values(local) as info, i}
             <div
                 class="justify-evenly bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
             >
-                <img
-                    class="rounded-t-lg hover:object-scale-down"
-                    src={image.imgurl}
-                    alt={image.name}
-                />
                 <div class="p-5">
                     <h5
                         class="mb-2 text-2xl font-bold truncate tracking-tight text-gray-900 dark:text-white"
                     >
-                        {image.name}
+                        {info.fishType}
                     </h5>
                     <p
                         class="mb-3 font-normal truncate text-gray-700 dark:text-gray-400"
                     >
-                        {image.attribution}
+                        {info.date}
                     </p>
 
                     <Button
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         on:click={() => (modals[i] = true)}>Visa mer</Button
                     >
-                    <Modal title={image.name} bind:open={modals[i]} autoclose>
-                        <img src={image.imgurl} alt={image.name} />
-
-                        <h1 class="text-xl">Om fisken</h1>
+                    <Modal
+                        title={info.fishType}
+                        bind:open={modals[i]}
+                        autoclose
+                        class="w-full h-max"
+                    >
+                        <h1 class="text-xl">Om din fångst</h1>
                         <p
                             class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
                         >
-                            {image.descripton}
+                            Fångad: {info.date}
+                        </p>
+                        <p
+                            class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+                        >
+                            Längd: {info.length}cm
+                        </p>
+                        <p
+                            class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+                        >
+                            Vikt: {info.weight}kg
                         </p>
                         <svelte:fragment slot="footer">
                             <Button>Tack och hej</Button>
@@ -49,6 +67,7 @@
             </div>
         {/each}
     </div>
-
-    <p class="text-s italic text-center text-gray-900 dark:text-white">Lägg till fler fiskar så synns de här</p>
+    <p class="text-s italic text-center text-gray-900 dark:text-white">
+        Lägg till fler fiskar så synns de här
+    </p>
 </Wrapper>
